b57da5376633337b9ffbf7196c4f34b4448ff4a9
Lekito <alexdossantosdias1212@gmail.com>
2025-03-06 06:45:49 -0300

    creating component for posts

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,31 +7,7 @@
 
     <div class="py-12">
         @foreach ($posts as $post)
-            <div class="mt-3">
-                <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                        <div class="p-6 text-gray-900 dark:text-gray-100">
-                            <!-- Cabeçalho do post -->
-                            <div class="flex justify-between items-start">
-                                <div>
-                                    <span class="text-gray-500 mr-3">Autor:</span>
-                                    <span class="text-blue-600">{{ $post->user->name }}</span>
-                                </div>
-                                <div class="text-right">
-                                    <span class="text-gray-500 mr-3">Criado em:</span>
-                                    <span>{{ $post->created_at }}</span>
-                                </div>
-                            </div>
-
-                            <!-- Conteúdo do post -->
-                            <div class="mt-4">
-                                <h1 class="text-xl font-bold">{{ $post->title }}</h1>
-                                <p class="mt-3">{{ $post->content }}</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <x-post-component :post="$post" />
         @endforeach
     </div>
 
